9b41033bb7269ac5197a2695d98c4314c47215fb
rtaraboo <rtarabocchia@gmail.com>
2022-07-21 15:44:18 -0400

    Added google sign in and user database

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopUp, creatUserDocumentFromAuth  } from "../../utilities/firebase/firebase.utilities";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopUp();
+    const userDocRef = await creatUserDocumentFromAuth(user);
+  };
   return (
     <div>
       <h1>SIGN IN PAGE</h1>
+      <button onClick={logGoogleUser}>Sign In With Google</button>
     </div>
   );
 };
---
+++ src/utilities/firebase/firebase.utilities.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiXokbI9HyKKP8YvglPlErsDw5y_QQmsk",
+  authDomain: "crown-clothing-db-a4b99.firebaseapp.com",
+  projectId: "crown-clothing-db-a4b99",
+  storageBucket: "crown-clothing-db-a4b99.appspot.com",
+  messagingSenderId: "201123298206",
+  appId: "1:201123298206:web:533cc050e16b0f23e4f48f",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const creatUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
