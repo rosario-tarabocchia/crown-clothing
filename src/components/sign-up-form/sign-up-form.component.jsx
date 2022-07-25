6d71032ad9eab3dde6c3276c8e2e7782ae577e3b
rtaraboo <rtarabocchia@gmail.com>
2022-07-25 18:08:45 -0400

    Add button and button styling

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,8 @@ import {
   createUserDocumentFromAuth,
 } from "../../utilities/firebase/firebase.utilities";
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
   displayName: "",
@@ -48,8 +50,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign Up With Email and Passowrd</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign Up With Email and Passowrd</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -83,7 +86,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
